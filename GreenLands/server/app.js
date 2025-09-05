c2d4674dde43a09781ef97b1ecbbfe7ef3bcaeff
DOMOSH85 <157604004+DOMOSH85@users.noreply.github.com>
2025-09-05 16:43:38 +0300

    Delete GreenLands/server directory

--- GreenLands/server/app.js
+++
@@ -1,5 +0,0 @@
-const financeRoutes = require('./routes/finance');
-app.use('/api/finance', financeRoutes);
-const subsidiesRoutes = require('./routes/subsidies');
-app.use('/api/subsidies', subsidiesRoutes);
-module.exports = app; 
